603f92c4acdd29959b2d225602ea07881c910516
peppelinux <giuseppe.demarco@unical.it>
2021-01-01 19:12:18 +0100

    search engine tests: coverage up to 90%

--- src/cms/api/tests.py
+++ src/cms/api/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import logging
 
-# Create your tests here.
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.test import Client, RequestFactory, TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from cms.carousels.tests import CarouselUnitTest
+from cms.contexts.tests import ContextUnitTest
+from cms.medias.tests import MediaUnitTest
+from cms.menus.tests import MenuUnitTest
+from cms.pages.tests import PageUnitTest
+from cms.templates.tests import TemplateUnitTest
+
+from . models import *
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
