d0e32f4468d1c7a1ba79c56409d4c08ca3746980
peppelinux <giuseppe.demarco@unical.it>
2021-01-15 11:20:45 +0100

    Used By model in contexts

--- src/cms/contexts/admin.py
+++ src/cms/contexts/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 
-from . models import EditorialBoardEditors, EditorialBoardLocks, WebPath, WebSite
+from . models import (EditorialBoardEditors, 
+                      EditorialBoardLocks, 
+                      WebPath, 
+                      WebSite,
+                      EntryUsedBy)
 from . utils import fill_created_modified_by
 
 
 class AbstractCreatedModifiedBy(admin.ModelAdmin):
     readonly_fields = ('created_by', 'modified_by')
 
-    def save_model(self, request, obj, form, change):
+    def save_model(self, request, obj, form, change): # pragma: no cover
         fill_created_modified_by(request, obj)
         super().save_model(request, obj, form, change)
 
@@ -54,3 +58,11 @@ class EditorialBoardLocksAdmin(admin.ModelAdmin):
     list_filter = ('locked_time', )
     list_display = ('content_type', 'object_id',
                     'is_active', 'locked_time', 'locked_by')
+
+
+@admin.register(EntryUsedBy)
+class EntryUsedByAdmin(admin.ModelAdmin):
+    list_filter = ('created', )
+    list_display = ('content_type', 'content_object',
+                    'used_by_content_type', 'used_by_content_object')
+    # raw_id_fields = ('object_id', 'used_by_object_id')
--- src/cms/contexts/models.py
+++ src/cms/contexts/models.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from cms.contexts.utils import sanitize_path
-from cms.templates.models import TimeStampedModel
+from cms.templates.models import TimeStampedModel, CreatedModifiedBy
 
 from . import settings as app_settings
 from . exceptions import ReservedWordException
@@ -20,20 +20,6 @@ logger = logging.getLogger(__name__)
 CMS_CONTEXT_PERMISSIONS = getattr(settings, 'CMS_CONTEXT_PERMISSIONS',
                                   app_settings.CMS_CONTEXT_PERMISSIONS)
 CMS_PATH_PREFIX = getattr(settings, 'CMS_PATH_PREFIX', '')
-
-
-class CreatedModifiedBy(models.Model):
-    created_by = models.ForeignKey(get_user_model(),
-                                   null=True, blank=True,
-                                   on_delete=models.CASCADE,
-                                   related_name='%(class)s_created_by')
-    modified_by = models.ForeignKey(get_user_model(),
-                                    null=True, blank=True,
-                                    on_delete=models.CASCADE,
-                                    related_name='%(class)s_modified_by')
-
-    class Meta:
-        abstract = True
 
 
 class WebSite(models.Model):
@@ -266,3 +252,33 @@ class EditorialBoardLocks(models.Model): # pragma: no cover
 
     def __str__(self): # pragma: no cover
         return f'{self.content_type} {self.object_id}'
+
+
+class EntryUsedBy(models.Model):
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        verbose_name=_("content type"),
+        related_name="%(app_label)s_%(class)s_entry",
+    )
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+    used_by_content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        verbose_name=_("used by content type"),
+        related_name="%(app_label)s_%(class)s_usedby",
+    )
+    used_by_object_id = models.PositiveIntegerField()
+    used_by_content_object = GenericForeignKey()
+    
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = _("Entries Used By")
+
+    def __str__(self): # pragma: no cover
+        return (f'{self.content_type} {self.object_id} used by '
+                f'{self.usedby_content_type} {self.usedby_object_id}')
+    
