92c3b09639e0148a9d2e7b0cb1821e5bd1c71496
Francesco Filicetti <francesco.filicetti@unical.it>
2022-03-11 12:13:12 +0100

    fix: publications templatetag

--- src/cms/publications/templatetags/unicms_publications.py
+++ src/cms/publications/templatetags/unicms_publications.py
@@ -76,7 +76,8 @@ def load_publications_preview(context, template,
                               categories_csv=None,
                               exclude_categories=False,
                               tags_csv=None,
-                              exclude_negative_order=False):
+                              exclude_negative_order=False,
+                              exclude_single_category_label=False):
 
     if categories_csv:
         categories = [i.strip() for i in categories_csv.split(',')]
@@ -115,6 +116,7 @@ def load_publications_preview(context, template,
     data = {'publications': pub_in_context,
             'categories': categories,
             'exclude_categories': exclude_categories,
-            'request': request}
+            'request': request,
+            'exclude_single_category_label': exclude_single_category_label}
     return handle_faulty_templates(template, data,
                                    name='load_publications_preview')
